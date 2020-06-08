a4722f1a5a1710393b5c9ab94b4cdd06b63f199c
BenJilks <benjyjilks@gmail.com>
2020-06-08 14:02:48 +0100

    Renamed 'public' to 'docs'

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default
     input: 'src/index.ts',
     output:
     {
-        file: 'public/bundle.js',
+        file: 'docs/bundle.js',
         format: 'es',
         sourcemap: true
     },
